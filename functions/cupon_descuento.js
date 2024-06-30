bee64a7a62585df18a505c89e0cc6bacb0bc1290
David <product@edyflow.com>
2024-06-30 16:48:23 +0200

    Conexión valor cupón con función para envío.

--- functions/cupon_descuento.js
+++ functions/cupon_descuento.js
@@ -2,24 +2,27 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_TEST);
 const { queryStripeCliente } = require('./utils/fauna');
 
 //Aplica un descuento a una suscripción
-exports.handler = async (_event, context) => {
+exports.handler = async (event, context) => {
   //Usuario netlify
   const { user } = context.clientContext;
-
-  var descuentoAplicado = await descuentoSubs(user.sub);
+  console.log("Dentro ", user);
+  /*const { user } = context.clientContext;
+  console.log("usuario: ", user.sub);
+  var descuentoAplicado = await descuentoSubs(user.sub);*/
 
   return {
     statusCode: 200,
-    body: descuentoAplicado, //JSON.stringify(descuentoAplicado),
+    body: "fin", //descuentoAplicado, //JSON.stringify(descuentoAplicado),
   };
 };
 
 
 async function descuentoSubs(id_netlify) {
   //Recupera id del cliente en stripe con el id del cliente en netlify.
-  const clienteID = await queryStripeCliente(id_netlify);
+  //const clienteID = await queryStripeCliente(id_netlify);
+  console.log("descuentoSubs ", id_netlify);
 
-  const stripe = require('stripe')('sk_test_51OXmGhGAVjNy5dcWqdTRW9E1Eh3MyAspCBVlYYRiZQISneBAcRK0MC1c4WNBYwJbnhRuO4X8l1kwTeWaLpXamVb200Z0n4lQK8');
+  /*const stripe = require('stripe')('sk_test_51OXmGhGAVjNy5dcWqdTRW9E1Eh3MyAspCBVlYYRiZQISneBAcRK0MC1c4WNBYwJbnhRuO4X8l1kwTeWaLpXamVb200Z0n4lQK8');
   
   const subscription = await stripe.subscriptions.create({
     customer: '{{CUSTOMER_ID}}',
@@ -33,9 +36,9 @@ async function descuentoSubs(id_netlify) {
           coupon: 'free-period',
         },
       ],
-  });
+  });*/
 
-  const subscriptionU = await stripe.subscriptions.update(
+  /*const subscriptionU = await stripe.subscriptions.update(
     'sub_1ChorizoEnorme',
     {
       items: [
@@ -49,7 +52,8 @@ async function descuentoSubs(id_netlify) {
         },
       ],
     }
-  );
+
+  );*/
 
 
   return "Descuento";
